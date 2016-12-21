bf7dc37c2a1f920233bf60b097756b4156cc93ad
Martin Hoecker-Martinez <mhoecker@coas.oregonstate.edu>
2016-12-21 14:52:11 -0500

    download NSIDC sea ice datasets

---
+++ getseaice.sh
@@ -0,0 +1,4 @@
+wget -N ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/north/daily/data/NH_seaice_extent_final_v2.csv
+wget -N ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/south/daily/data/SH_seaice_extent_final_v2.csv
+wget -N ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/south/daily/data/SH_seaice_extent_nrt_v2.csv
+wget -N ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/north/daily/data/NH_seaice_extent_nrt_v2.csv
